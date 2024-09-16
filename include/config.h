415cf24609d24f06f9f4b87ddcca71ab09f2a619
Axel van Abkoude <axel.abkoude@gmail.com>
2024-09-16 10:39:10 +0200

    include one bit boolean logic

---
+++ include/config.h
@@ -0,0 +1,16 @@
+/* Real time libraries.  */
+#define HAVE_GETTIME_REALTIME
+
+/* #undef HAVE_MACH_CLOCK_GET_TIME */
+
+/* `mkdtemp' function */
+#define HAVE_MKDTEMP
+
+/* Do we need to link with -lm.  */
+/* #undef NEED_LIBM */
+
+/* Do we need to link with -lrt.  */
+/* #undef NEED_LIBRT */
+
+/* Support sac2c #pragma header feature */
+#define HAVE_HEADER_PRAGMA
